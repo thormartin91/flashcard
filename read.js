51218c4e9dcdd6a982ad89d2b994906e3c9c497f
thormartin91 <thorma@stud.ntnu.no>
2014-10-31 22:36:42 +0100

    Add read.js

---
+++ read.js
@@ -0,0 +1,32 @@
+// This file reads the content of 'file.txt' into flashcard.infosys
+// The structure of 'file.txt' should be:
+// title - description  \n  title - description
+// MongoDB currently running at localhost
+
+var fs = require('fs'), 
+	filename = 'file.txt',
+	mongo = require('mongodb').MongoClient
+  ;
+// Connect to DB
+mongo.connect('mongodb://127.0.0.1/flashcard', function(err, db) {
+	if(err) throw err;
+
+	// connect to collection
+	var col = db.collection('infosys');
+
+	// Read file  
+	fs.readFile(filename, 'utf8', function(err, data) {
+		if (err) throw err;
+
+		var listOfData = data.toString().split('\n')
+		for(var i in listOfData) {
+			var array = listOfData[i].split(' - '),
+				title = array[0],
+				description = array[1];
+
+			// Insert to DB
+			col.insert({title: title, description: description}, function() {});
+			console.log('Inserted: ', title, ' - ', description);
+		};
+	});
+});
